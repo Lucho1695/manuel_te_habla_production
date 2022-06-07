40fe3831851a9bdd63b098e17bf6c1b266cff540
Luis Garcia <luiscarlosgarcialucho@gmail.com>
2022-06-06 19:51:26 -0500

    ajuste

--- app/models/subcategory.rb
+++ app/models/subcategory.rb
@@ -5,18 +5,6 @@ class Subcategory < ApplicationRecord
 
   validates :title, presence: true
 
-  after_create :validate_params
-  before_update :update_params
-
-  def validate_params
-    self.subcategories_image = "#{self.id}"
-    self.save
-  end
-
-  def update_params
-    self.subcategories_image = "#{self.id}"
-  end
-
   def is_image?
     ['image/jpeg', 'image/pjpeg', 'image/gif', 'image/png', 'image/x-png', 'image/jpg', 'image/webp'].include?(self.subcategories_image.content_type)
   end
