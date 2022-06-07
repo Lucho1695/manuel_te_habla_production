f513d5fb876946b0cf7f17a629e7223331ad4dbd
Luis Garcia <luiscarlosgarcialucho@gmail.com>
2022-06-06 20:52:49 -0500

    ajuste

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -80,7 +80,7 @@ class CategoriesController < ApplicationController
         @category.category_image = "#{@category.id}"
         @category.save!
 
-        format.html { redirect_to @category, notice: 'Category was successfully created.' }
+        format.html { redirect_to categories_path, notice: 'Category was successfully created.' }
         format.json { render :show, status: :created, location: @category }
       else
         format.html { render :new }
